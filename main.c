4594b9c3d402715d1793d90cb8846e56c395305a
Nikolas Morado <nikolasmorado@gmail.com>
2026-04-07 02:34:57 -0400

    feat: subtasks, structure for next features

--- main.c
+++ main.c
@@ -39,7 +39,8 @@
  *               └── navbar
  *                   └── .taskd
  *
- *      Each file is just the flat task details.
+ *      Each file is has some hugo style frontmatter at the top for some things
+ *      I plan to add, and then has a list of subtasks as a checklist
  *
  *
  * ARGUMENTS
@@ -119,25 +120,25 @@ main(int argc, char *argv[]) {
 
         if (argc == 2) {
                 if (stat(argv[1], &st) == 0 && S_ISDIR(st.st_mode)) {
-                                ret = chdir(argv[1]);
+                        ret = chdir(argv[1]);
 
-                                if (ret == -1) {
-                                        return -1;
-                                } 
+                        if (ret == -1) {
+                                return -1;
+                        } 
 
-                                ret = system(fzfcmd);
+                        ret = system(fzfcmd);
 
-                                if (ret == 1) {
-                                        printf ("no tasks\n");
-                                        return 0;
-                                }
-
-                                if (ret == 2 || ret == 126 || ret == 127) {
-                                        printf ("fzf error: %d", ret);
-                                        return -1;
-                                }
-
+                        if (ret == 1) {
+                                printf ("no tasks\n");
                                 return 0;
+                        }
+
+                        if (ret == 2 || ret == 126 || ret == 127) {
+                                printf ("fzf error: %d", ret);
+                                return -1;
+                        }
+
+                        return 0;
                 }
 
                 char fp[strlen(argv[1] + 8)];
@@ -202,6 +203,58 @@ main(int argc, char *argv[]) {
                 }
         }
 
+        if (strcmp(argv[1], "done") == 0) {
+                char fp[strlen(argv[2]) + 8];
+                sprintf(fp, "%s/.taskd", argv[2]);
+                FILE *f = fopen(fp, "r+");
+
+                if (!f) {
+                        printf("no file\n");
+                        return -1;
+                }
+
+                fseek(f, 0, SEEK_END);
+                long sz = ftell(f);
+
+                rewind(f);
+
+                char *buf = malloc(sz + 1);
+                size_t fret = fread(buf, 1, sz, f);
+
+                if (fret != (size_t)sz) {
+                        free(buf);
+                        printf("read err\n");
+                        return -1;
+                }
+
+                buf[sz] = '\0';
+
+                char *p = buf;
+                int c = 0;
+
+                while (*p) {
+                        if(
+                                strncmp(p, "- [", 3) == 0 &&
+                                c++ == atoi(argv[3])
+                        ) {
+                                p[3] = 'x';
+                                break;
+                        }
+                        while (*p && *p != '\n') p++;
+                        if (*p) p++;
+                }
+
+                rewind(f);
+                fwrite(buf, 1, sz, f);
+                fflush(f);
+
+                free(buf);
+                fclose(f);
+
+                return 0;
+        }
+
+
         char *p = argv[1];
         while (*p != '\0') {
                 if (*p == '/') {
@@ -215,15 +268,22 @@ main(int argc, char *argv[]) {
         mkdir(argv[1], 0755);
 
         char fp[strlen(argv[1]) + 8];
+
         sprintf(fp, "%s/.taskd", argv[1]);
-        FILE *f = fopen(fp, "w+");
+
+        int e = stat(fp, &st);
+        FILE *f = fopen(fp, "a+");
 
         if (!f) {
                 printf("no file\n");
                 return -1;
         }
 
-        fprintf(f, "%s", argv[2]);
+        if (e != 0) {
+                fprintf(f, "priority: \nblockers: \n---\n");
+        }
+
+        fprintf(f, "- [ ] %s\n", argv[2]);
 
         return 0;
 }
